92e6cd6f50e2c7a849613556a06e86db65797a02
azhou95 <andrewzhou95@hotmail.com>
2021-12-20 12:02:11 +0000

    fix to second expression

--- simplified_case.py
+++ simplified_case.py
@@ -1,15 +1,16 @@
+import matplotlib.pyplot as plt
+
 from utils import f, sum_freq, sum_by_freq
 
 
 def calc_expectation(threshold: int):
     total_cards = sum(f.values())
     e = (sum_freq(-2, threshold) / total_cards) * (sum_by_freq(-2, threshold) / sum_freq(-2, threshold)) + \
-        (sum_freq(threshold, 12) / total_cards) * (sum_by_freq(-2, 12) / sum_freq(-2, 12))
+        (sum_freq(threshold + 1, 12) / total_cards) * (sum_by_freq(-2, 12) / sum_freq(-2, 12))
     return e
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
     thresholds = range(min(f), max(f) + 1)
     vals = [calc_expectation(t) for t in thresholds]
 
